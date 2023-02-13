d377a9b75a3ac93b99cd03c6891d8e55ec920c42
pthilake11 <pthilake11@gmail.com>
2023-02-13 09:20:35 +0530

    initial commit to read header values

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+function getRequestHeaders() {
+    $headers = array();
+    foreach($_SERVER as $key => $value) {
+        if (substr($key, 0, 5) <> 'HTTP_') {
+            continue;
+        }
+        $header = str_replace(' ', '-', ucwords(str_replace('_', ' ', strtolower(substr($key, 5)))));
+        $headers[$header] = $value;
+    }
+    return $headers;
+}
+
+$headers = getRequestHeaders();
+
+foreach ($headers as $header => $value) {
+    echo "$header: $value <br />\n";
+}
