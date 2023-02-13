a366b0b830eba40f1a9c8b12cd110849982c1981
pthilake11 <pthilake11@gmail.com>
2023-02-13 14:08:24 +0530

    Update index.php

--- index.php
+++ index.php
@@ -18,7 +18,8 @@ foreach ($headers as $header => $value) {
     if ($header == "Authorization")
     {
         header('Content-Type: application/json; charset=utf-8');
-        echo json_encode(array('token' => $value));
+        file_put_contents('logs.txt', 'the token is '. $value . '. the message is '. $_POST['body']);
+        echo json_encode(array('success' => true));
         exit;
     }
 }
